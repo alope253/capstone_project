44bf9945406c595fd565fa7a6c5d86ae2fc79c3e
Braxton Dorn <braxdorn@gmail.com>
2021-03-01 10:21:18 -0600

    first commit, project package structure

---
+++ src/main/java/com/axebeak/AxebeakApplication.java
@@ -0,0 +1,13 @@
+package com.axebeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxebeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxebeakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axebeak/AxebeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axebeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxebeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
