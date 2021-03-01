183c4f38956caed9c41245f8937d1e5a8e636936
Christina Vicente <christina.vicente@hcl.com>
2021-03-01 13:38:48 -0500

    please let this work

--- src/main/java/com/axebeak/controllers/LoginController.java
+++ src/main/java/com/axebeak/controllers/LoginController.java
@@ -1,24 +1,5 @@
 package com.axebeak.controllers;
 
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
 public class LoginController {
-
-
-    @PostMapping("login")
-    public String submitLogin(){
-
-        return "login";
-    }
-
-    @GetMapping("login")
-    public String loadLogin(){
-
-        return "display-songs";
-    }
 
 }
--- src/main/java/com/axebeak/controllers/UserController.java
+++ src/main/java/com/axebeak/controllers/UserController.java
@@ -1,27 +1,5 @@
 package com.axebeak.controllers;
 
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
 public class UserController {
-
-    @PostMapping("user")
-    public String addToCart(){
-
-        return "user";
-    }
-
-    @GetMapping("user")
-    public String showCart(){
-        return "user";
-    }
-
-
-
-
-
 
 }
