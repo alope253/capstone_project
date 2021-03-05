cb0c228e9bb72eff8fdd7355b080597787a7de82
Braxton Dorn <braxdorn@gmail.com>
2021-03-05 12:29:30 -0600

    committing changes made juuuust before presentation

--- src/main/java/com/axebeak/services/ProductService.java
+++ src/main/java/com/axebeak/services/ProductService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.axebeak.model.Product;
-import com.axebeak.model.Product.PRODUCTTYPE;
 import com.axebeak.repositories.ProductRepository;
 
 
